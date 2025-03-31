373ffb1fdb9a137de891bdfea4d3c1563b9c4c1c
GabiKaminsky145 <gavrielk@post.bgu.ac.il>
2025-03-31 23:18:50 +0300

    update

--- db-demo.js
+++ db-demo.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "GuestsStatus",
-    password: "dchuji123",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 // Get all guests with their category, RSVP status, and number of attendees
