441ce524c5e941cd7e5a26068095ca7cc6f7195d
GabiKaminsky145 <gavrielk@post.bgu.ac.il>
2025-04-04 15:50:28 +0300

    update pm2 service

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+// ecosystem.config.js
+module.exports = {
+    apps: [
+      {
+        name: "whatsapp-bot",
+        script: "whatsapp-bot.js",
+        watch: false,
+      },
+    ],
+  };
+  
